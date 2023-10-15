af56c0b310f97ff33686f7dc223cab7e45568a4c
shreyasraok <147699881+shreyasraok@users.noreply.github.com>
2023-10-15 17:07:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+// prompt("Enter your Name","");
+// console.log("This is console");
+// console.log(6 + 5);
+//  document.write("<h1> Javascript </h1>");
+
+// var num1=5;
+// var num1=6;
+// num1=8
+// console.log(num1);
+
+// let num2 = 9
+
+// num2 = 11
+// console.log(num2);
+
+// const num3 = 10
+
+// console.log(num3);
+// console.log(3**2);
+// let x=5
+// x++
+// console.log(x);
+// x---
+// console.log(x);
+
+// let num1=5
+// let num2 = "5"
+// console.log(num1 == num2);
+// console.log(num1 === num2);
+
+// function mul(n1,n2,n3,n4) {
+//     return n1 * n2 * n3 * n4;
+// }
+// document.write(mul(4,5,6,7));
+
+// function greet(){
+//     alert("Hello Everyone");
+// }
+
+// let age = 10;
+
+// if (age >=18) {
+//    document.write("Eligible for vote");
+
+// } else {
+//    document.write("Not Eligible");
+//}
+
+// for (let num = 10; num >= 1; num--) {
+//     console.log(num);
+//}
+
+
+
+// Dom - Document Object Model
+// Dom Manipulation Methods
+// document.getElementById()|
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelector()
+// document.querySelectorAll()
+
+// let el=document.getElementById("head").innerHTML;
+//console.log(el);
+
+let ele=document.querySelector("#head").innerHTML;
+console.log(ele);
+
