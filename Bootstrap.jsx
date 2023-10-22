130f82093405579b229551845a42c72449c2763e
shreyasraok <147699881+shreyasraok@users.noreply.github.com>
2023-10-22 20:46:15 +0530

    Add files via upload

---
+++ Bootstrap.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
+
+function Bootstrap(){
+    return(
+      <>
+        <h1 className="text-primary text-center">Bootstrap in React</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero placeat saepe porro repellendus magni rerum vitae temporibus id amet autem! Dolor sunt magnam quia necessitatibus iste nam blanditiis eum omnis.
+        </p>
+      </>
+
+
+    );
+}
+export default Bootstrap;
---
+++ Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header(){
+    return(
+         <div>
+            <h1>Nathuram Godse</h1>
+            <img 
+            src="https://s.yimg.com/zb/imgv1/6fb425b4-82e3-329e-944b-3f59388c1b1b/t_500x300"
+            alt="loading..."
+            />
+        </div>
+          )
+}
+export default Header;
