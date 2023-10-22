9c12c1c47d90fe6c3afd5ebb92515541c6e954b1
shreyasraok <147699881+shreyasraok@users.noreply.github.com>
2023-10-22 20:26:34 +0530

    Add files via upload

---
+++ Body.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const Body = () =>
+{
+  return(
+    <>
+      <hr />
+      <h3>about</h3>
+      <p>EA Sports</p>
+</>
+  );
+};
+export default Body;
---
+++ Footer.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+class Footer extends Component
+{
+  reader() {
+      return(
+          <>
+          <hr />
+          <h3>contact </h3>
+          <p>
+          <b>mobile</b>23456789
+          </p>
+          <p>
+          <b>email</b>
+          </p>
+          </>
+        );
+  }
+}
+export default Footer;
